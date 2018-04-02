d1574365af655353804343854659a4962688a412
BigstepRapid <BigstepRapid@bigstep.com>
2018-04-02 13:09:55 +0000

    rapid generated update

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from distutils.core import setup
 from setuptools import find_packages
 
 version = '2.11'
-name = 'metalcloud-api-client'
-url = 'https://github.com/bigstepinc/metalcloud-api-client-python'
+name = 'metal-cloud-sdk'
+url = 'https://github.com/bigstepinc/metal-cloud-sdk-python2.x.git'
 
 setup(
     name=name,
     packages=find_packages(),
     version=version,
-    description='Api client for Metal Cloud infrastructure',
+    description='SDK for the Metal Cloud infrastructure',
     author='Bigstep Inc.',
     author_email='bsiteam@bigstep.com',
     url=url,
     download_url= url + '/tarball/' + version,
-    keywords=["metal", "cloud", "api", "client"],
+    keywords=["metal", "cloud", "sdk"],
     install_requires = [ 'jsonrpc2-base==0.7' ],
     classifiers=[]
 )
